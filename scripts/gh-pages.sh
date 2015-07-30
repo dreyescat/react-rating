c8cdeb05ae1ba951cebfce3a72e831a688aa5611
dreyescat <dreyes.cat@gmail.com>
2015-07-30 20:19:10 +0200

    Deploy gh-pages based on remote repository

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# Clone local repo to dist and point to gh-pages branch
+# Clone remote repository to dist and point to gh-pages branch
 # http://stackoverflow.com/questions/1911109/clone-a-specific-git-branch
 # Clone gh-pages preventing fetching of all branches add --single-branch
-git clone -b gh-pages . dist
+git clone -b gh-pages https://github.com/dreyescat/react-rating.git dist
 
-# Checkout changes from local master
-# Using git without having to change directory
+# Checkout changes from master
+# Using git without having to change directory (-C dist)
 # http://stackoverflow.com/questions/5083224/git-pull-while-not-in-a-git-directory
 git -C dist checkout origin/master -- :/index.html :/lib
 
@@ -16,15 +16,14 @@ rsync -avR node_modules/bootstrap/dist dist
 rsync -avR node_modules/font-awesome/css dist
 rsync -avR node_modules/font-awesome/fonts dist
 
-# Commit with the last commit message from master
-git -C dist commit -m "$(git log master -1 --pretty=%B)"
+# Add synched dependencies changes
+git -C dist add .
 
-# This only pushes to the origin, that is, the local repo from where
-# this temporary repo was cloned
-#git -C dist push origin gh-pages
+# Commit with the last commit message from master (usually release version)
+git -C dist commit -m "$(git log origin/master -1 --pretty=%B)"
 
 # Push to remote gh-pages
-git -C dist push https://github.com/dreyescat/react-rating.git gh-pages
+git -C dist push origin gh-pages
 
 # Remove temporary dist folder
 rm -rf dist
