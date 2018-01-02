844368196d7d34c98efa095d4b5adb724b415627
Dimitris Karagiannis <mitch.karajohn@gmail.com>
2018-01-02 18:10:13 +0200

    Fix issue #64

--- src/RatingAPILayer.jsx
+++ src/RatingAPILayer.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Style from './utils/style';
 import Rating from './Rating';
 import noop from './utils/noop';
